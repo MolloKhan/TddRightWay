4b299641b843565ac0326fc3b886b71925b3dec9
Diego Aguiar <barret91@hotmail.com>
2021-11-03 16:06:11 -0700

    test nothing

---
+++ tests/NameInverterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NameInverterTest extends TestCase
+{
+    public function testNothing()
+    {
+        self::assertTrue(true);
+    }
+}
