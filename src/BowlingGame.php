484a559ed478ca4fd8fb0b48d13ca83da4f1e8fe
Diego Aguiar <barret91@hotmail.com>
2021-11-08 18:43:40 -0700

    Making test pass

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -4,16 +4,29 @@ namespace App;
 
 class BowlingGame
 {
-    private int $score = 0;
+    private array $rolls = [];
+    private int $currentRoll = 0;
 
     public function roll(int $pins)
     {
-        $this->score += $pins;
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
     public function score(): int
     {
-        // "fake it till you make it" technique
-        return $this->score;
+        $score = 0;
+        $frameIndex = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            // spare
+            if ($this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] === 10) {
+                $score += 10 + $this->rolls[$frameIndex + 2];
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+            }
+
+            $frameIndex += 2;
+        }
+
+        return $score;
     }
 }
