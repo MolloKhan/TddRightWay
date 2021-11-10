69dd28f29002bc2218b508f322127331b8592a1c
Diego Aguiar <barret91@hotmail.com>
2021-11-10 14:38:37 -0700

    Implementation One Strike

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -7,6 +7,12 @@ class BowlingGame
     private array $rolls = [];
     private int $currentRoll = 0;
 
+    public function __construct()
+    {
+//        $this->rolls = array_fill(0, 21, null);
+        $this->rolls = (new \SplFixedArray(21))->toArray();
+    }
+
     public function roll(int $pins)
     {
         $this->rolls[$this->currentRoll++] = $pins;
@@ -17,13 +23,16 @@ class BowlingGame
         $score = 0;
         $frameIndex = 0;
         for ($frame = 0; $frame < 10; $frame++) {
-            if ($this->isSpare($frameIndex)) {
+            if ($this->rolls[$frameIndex] === 10) { // strike
+                $score += 10 + $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+                $frameIndex++;
+            } else if ($this->isSpare($frameIndex)) {
                 $score += 10 + $this->rolls[$frameIndex + 2];
+                $frameIndex += 2;
             } else {
                 $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+                $frameIndex += 2;
             }
-
-            $frameIndex += 2;
         }
 
         return $score;
