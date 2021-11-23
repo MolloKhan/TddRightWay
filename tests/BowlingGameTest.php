eaf3eba381f79cc3eaf3d6f53cb6073976c38e66
Diego Aguiar <barret91@hotmail.com>
2021-11-22 17:37:41 -0700

    [tuts-apply-diff] Applying existing diff file for step bowlingkata-gutter-gamer-test

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -2,9 +2,19 @@
 
 namespace App\Tests;
 
+use App\BowlingGame;
 use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
 {
-    
+    public function testGutterGame()
+    {
+        $game = new BowlingGame();
+
+        for ($i = 0; $i < 20; $i++) {
+            $game->roll(0);
+        }
+
+        self::assertEquals(0, $game->score());
+    }
 }
