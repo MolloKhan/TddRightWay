632231892756adaf2fbd79d0e2e9bc3612177dbc
Diego Aguiar <barret91@hotmail.com>
2021-11-22 18:04:39 -0700

    Remove tests

--- tests/WordWrapperTest.php
+++ tests/WordWrapperTest.php
@@ -10,24 +10,8 @@ class WordWrapperTest extends TestCase
     public function testShouldWrap()
     {
         $wordWrapper = new WordWrapper();
-        $result = $wordWrapper->wrap('word word', 4);
+        $result = $wordWrapper->wrap('', 1);
 
-        self::assertEquals("word\nword", $result);
-    }
-
-    public function testShouldNotWrap()
-    {
-        $wordWrapper = new WordWrapper();
-        $result = $wordWrapper->wrap('a word', 6);
-
-        self::assertEquals("a word", $result);
-    }
-
-    public function testBreakLineAcrossWord()
-    {
-        $wordWrapper = new WordWrapper();
-        $result = $wordWrapper->wrap('this is a test', 4);
-
-        self::assertEquals("this\nis a\ntest", $result);
+        self::assertEquals('', $result);
     }
 }
