709b7bce954fc2ab42b2337f38536d328ee17c5b
Diego Aguiar <barret91@hotmail.com>
2021-11-22 17:36:39 -0700

    [tuts-apply-diff] Applying existing diff file for step nameinverter-integration-test

--- src/NameInverter.php
+++ src/NameInverter.php
@@ -10,24 +10,48 @@ class NameInverter
             return '';
         }
 
-        $nameParts = preg_split('/\s+/', trim($name));
+        return $this->formatName(
+            $this->removeHonorifics(
+                $this->splitBySpaces($name)
+            )
+        );
+    }
 
+    private function splitBySpaces(string $name): array
+    {
+        return preg_split('/\s+/', trim($name));
+    }
+
+    private function removeHonorifics(array $nameParts): array
+    {
         if (count($nameParts) > 1 && $this->isHonorific($nameParts[0])) {
             array_shift($nameParts);
         }
 
-        if (count($nameParts) === 1) {
-            return $nameParts[0];
-        }
-
-        $postNominals = $this->getPostNominals($nameParts);
-
-        return trim(sprintf('%s, %s %s', $nameParts[1], $nameParts[0], $postNominals));
+        return $nameParts;
     }
 
     private function isHonorific(string $word): bool
     {
         return preg_match('/mr|mrs|ms/', str_replace('.', '', strtolower($word)));
+    }
+
+    private function formatName(array $nameParts): string
+    {
+        if (count($nameParts) === 1) {
+            return $nameParts[0];
+        }
+
+        return $this->formatNameWithPostNominals($nameParts);
+    }
+
+    private function formatNameWithPostNominals(array $nameParts): string
+    {
+        return trim(sprintf('%s, %s %s',
+            $nameParts[1],
+            $nameParts[0],
+            $this->getPostNominals($nameParts)
+        ));
     }
 
     private function getPostNominals(array $nameParts): string
