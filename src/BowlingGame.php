2770f443998c300de83b2b7faeef078e5740470c
Diego Aguiar <barret91@hotmail.com>
2021-12-01 14:10:47 -0700

    [tuts-apply-diff] Applying existing diff file for step bowlingkata-gutter-gamer-test

---
+++ src/BowlingGame.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class BowlingGame
+{
+    public function roll(int $int)
+    {
+    }
+
+    public function score(): int
+    {
+        // "fake it till you make it" technique
+        return 0;
+    }
+}
