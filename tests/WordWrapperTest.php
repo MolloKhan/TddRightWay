04bdae4f4c51c301dea41bb21a95665b9cd1b429
Diego Aguiar <barret91@hotmail.com>
2021-12-01 14:10:47 -0700

    [tuts-apply-diff] Applying existing diff file for step wordwrap-wrong-start

---
+++ tests/WordWrapperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class WordWrapperTest extends TestCase
+{
+    public function testNothing()
+    {
+        self::assertTrue(true);
+    }
+}
